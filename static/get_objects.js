452b1c5aaf3d3d42ddc3acaf2d62848a1ba62ba1
Diego Lopez <ndzerglink@live.jp>
2022-09-20 17:08:42 +0900

    Organized dir and sep JScode from HTML

---
+++ static/get_objects.js
@@ -0,0 +1,48 @@
+let apiKey = 'BxfX2Ho5Logh0lWk0AqY1MtZBE2SJVNGPLKfT4Ze'
+let url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-07-15&end_date=2021-07-15&api_key=' + apiKey;
+
+let vm = new Vue({
+  el: '#app',
+  data: {
+    asteroids: [],
+    showSummary: true,
+  },
+  computed: {
+    numAsteroids() {
+      return this.asteroids.length;
+    },
+    closestObject() {
+      let neosHavingData = this.asteroids.filter(neo => neo.close_approach_data.length > 0)
+      let simpleNeos = neosHavingData.map(neo => {
+        return { name: neo.name, miles: neo.close_approach_data[0].miss_distance.miles }
+      });
+      let sortedNeos = simpleNeos.sort((a, b) => a.miles - b.miles)
+      return sortedNeos[0].name
+    }
+  },
+  methods: {
+    fetchAstroids() {
+      axios.get(url)
+        .then((res) => {
+          vm.asteroids = res.data.near_earth_objects['2021-07-15']
+        })
+    },
+    getCloseApproachDate(a) {
+      if (a.close_approach_data.length > 0) {
+        return a.close_approach_data[0].close_approach_date_full
+      } else {
+        return 'NA'
+      }
+    },
+    remove(index) {
+      this.asteroids.splice(index, 1)
+    },
+    isMissingData(asteroid) {
+      return asteroid.close_approach_data.length == 0
+    }
+  },
+
+  created() {
+    this.fetchAstroids()
+  },
+});
