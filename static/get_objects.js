8e1f9ec20b6e53df356e15c68f95cf6a598ce260
DLopez <26180179+ndlopez@users.noreply.github.com>
2023-09-18 20:43:39 +0900

    RM axios stupid dependency on get_objects.js

--- static/get_objects.js
+++ static/get_objects.js
@@ -1,14 +1,12 @@
 let apiKey = 'BxfX2Ho5Logh0lWk0AqY1MtZBE2SJVNGPLKfT4Ze'
 const timeNow = new Date();
 //const day = timeNow.getDate(); //date today
-var monty = timeNow.getMonth() + 1; //starts from 0:Jan
-if(monty < 10){
-    monty = "0"+monty;
-}
-const myDate = "2022-"+monty+"-"+timeNow.getDate();
+const monty = timeNow.getMonth() + 1; //starts from 0:Jan
+
+const myDate = `2023-${monty.toString().padStart(2, '0')}-${timeNow.getDate()}`;
 //console.log(day, monty,myDate);
 
-let url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date='+myDate+'&end_date='+myDate+'&api_key=' + apiKey;
+const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${myDate}&end_date=${myDate}&api_key=${apiKey}`;
 
 let vm = new Vue({
   el: '#app',
@@ -34,10 +32,15 @@ let vm = new Vue({
   },
   methods: {
     fetchAstroids() {
+      fetch(url)
+      .then((res)=> res.json())
+      .then((data) => {vm.asteroids = data.near_earth_objects[myDate]})
+      .catch(error=>console.log("got:",error))
+      /*useless axios func
       axios.get(url)
         .then((res) => {
           vm.asteroids = res.data.near_earth_objects[myDate]
-        })
+        })*/
     },
     getCloseApproachDate(a) {
       if (a.close_approach_data.length > 0) {
